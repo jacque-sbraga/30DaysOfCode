a53d36396a84044e4e082ed9cdbe9eb01e5c2ba0
Jacqueline Santos <jaquelinesantosbraga@gmail.com>
2024-01-30 15:22:12 -0300

    solved day 1 and day 2

---
+++ Day02/Day02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = int.Parse(Console.ReadLine()!);
+            double numberB = double.Parse(Console.ReadLine()!);
+            string text = Console.ReadLine()!;
+
+            // the variables i, d and s are already declared in the submission platform.
+            //Console.WriteLine(numberA + i);
+            //Console.WriteLine((numberB + d).ToString("0.0"));
+            //Console.WriteLine(s + text);
+            Console.WriteLine(numberA + numberB);
+            Console.WriteLine((numberB + numberB).ToString("0.0"));
+            Console.WriteLine("HackerRank " + text);
+        }
+    }
+}
